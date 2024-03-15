667d606b205c182e56d75b5d3d8f130a34546530
Dilesha13 <tharushidilesha1@gmail.com>
2024-03-15 13:54:10 +0530

    Add necessary imports
    Add necessary imports for text processing and natural language processing tasks

--- Question Generation/questiongeneration.py
+++ Question Generation/questiongeneration.py
@@ -2,3 +2,24 @@ import numpy as np
 import pandas as pd 
 import time
 import torch
+from transformers import T5ForConditionalGeneration,T5Tokenizer
+import random
+import spacy
+import zipfile
+import os
+import json
+from sense2vec import Sense2Vec
+import requests
+from collections import OrderedDict
+import string
+import pke
+import nltk
+from nltk import FreqDist
+nltk.download('brown')
+nltk.download('stopwords')
+nltk.download('popular')
+from nltk.corpus import stopwords
+from nltk.corpus import brown
+from similarity.normalized_levenshtein import NormalizedLevenshtein
+from nltk.tokenize import sent_tokenize
+from flashtext import KeywordProcessor
