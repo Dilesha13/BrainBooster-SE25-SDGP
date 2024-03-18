c5a35d629003e60c0b5080ff8ea0fd0a6efc2183
Nipuni Pathirana <nipuni.20220370@iit.ac.lk>
2024-03-19 00:23:43 +0530

    update Modal.js

--- recommendation-page/src/Components/Modal.js
+++ recommendation-page/src/Components/Modal.js
@@ -1,20 +1,25 @@
 import React from "react";
-const Modal=()=>{
+const Modal=({show,item,onclose})=>{
+    if(!show)
+    {
+        return null;
+    }
+    let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
     return(
         <>
         <div className="overlay">
             <div className="overlay-inner">
-                <button className="close"><i class="fa-light fa-square-xmark"></i></button>
+                <button className="close" onClick={onclose}><i class="fa fa-window-close" aria-hidden="true"></i></button>
                 <div className="inner-box">
-                    <img src="./images/book01.jpeg" alt="book"/>
+                    <img src={thumbnail} alt="book"/>
                     <div className="info">
-                        <h1>ffffffffffffffffffffffffffffffffffffffffffff</h1>
-                        <h3>sssssssssssssss</h3><br/>
-                        <h4>eeeeeeeeeeeeee<span>345566</span></h4><br/>
-                        <a href="#"><button>More</button></a>
+                        <h1>{item.volumeInfo.title}</h1>
+                        <h3>{item.volumeInfo.authors}</h3>
+                        <h4>{item.volumeInfo.publisher}<span>{item.volumeInfo.publishedDate}</span></h4><br/>
+                        <a href={item.volumeInfo.previewLink}><button>More</button></a>
                     </div>
                 </div>
-                <h4 className="description">uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu</h4>
+                <h4 className="description">{item.volumeInfo.description}</h4>
             </div>
         </div>
         </>
