f4a7787fae9effb9457d9fedf27febfb20337769
Nipuni Pathirana <nipuni.20220370@iit.ac.lk>
2024-03-21 01:10:29 +0530

    create text paste area and submit button-app.js

--- pdf-summarizer/src/App.js
+++ pdf-summarizer/src/App.js
@@ -14,9 +14,15 @@ function App() {
           </a>
         </div>
         <div className="flex flex-col items-center justify-center mt-4 p-4">
-          <h1 className="text-3xl text-white text-center leading-10 font-senibold">Summarizer with<br/><span className="text-5xl font-bold text-cyan-500">OpenAI</span></h1>
+          <h1 className="text-3xl text-white text-center leading-10 font-semibold">Summarizer with<br/><span className="text-5xl font-bold text-cyan-500">OpenAI</span></h1>
           <p className="mt-5 text-lg text-gray-500 sm:text-xl text-center max-w-2xl">Simply upload your document and get a quick summary using OpenAI GPT Summarizer </p>
-
+        </div>
+        <div className="flex flex-col w-full items-center justify-center mt-5  ">
+          <textarea placeholder="Paste doc content here ..."
+          rows={7} className="block w-full md:w-[650px] rounded-md border border-slate-700 bg-slate-800 p-2 text-sm shadow-lg font-medium text-white
+          focus:border-gray-500 focus:outline-none focus:ring-0"> 
+          </textarea>
+          <button className="mt-5 bg-blue-500 px-5 py-2 text-white text-md font-semibold cursor-pointer rounded-md">Submit</button>
         </div>
       </div>    
     </div>
