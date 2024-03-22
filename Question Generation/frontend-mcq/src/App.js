f2d52257203fc28d93269fe9156b51d9cfd02c9f
Dilesha13 <tharushidilesha1@gmail.com>
2024-03-22 16:21:25 +0530

    Updat App.js
    Import necessary modules from 'react-router-dom' and custom components.

---
+++ Question Generation/frontend-mcq/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './Components/Home';
+import QuizInstructions from './Components/quiz/QuizInstructions';
+import Play from './Components/quiz/Play';
+import QuizSummary from './Components/quiz/QuizSummary';
