67e60943ff71fe49b27cdd786a045f6d429c7991
Dilesha13 <tharushidilesha1@gmail.com>
2024-03-22 16:24:01 +0530

    Update QuizInstructions.js
    The code snippet imports necessary modules from React and other libraries, and it also imports some image assets.

---
+++ Question Generation/frontend-mcq/src/Components/quiz/QuizInstructions.js
@@ -0,0 +1,8 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+import answer from '../../assets/img/answer.png';
+import fiftyFifty from '../../assets/img/fiftyFifty.PNG';
+import hints from '../../assets/img/hints.PNG';
+import options from '../../assets/img/options.PNG';
