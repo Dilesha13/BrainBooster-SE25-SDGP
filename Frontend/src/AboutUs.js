c05056c52c62402fecb8949a97dfe342c7501fc6
ImashiIIT <imashi.20221515@iit.ac.lk>
2024-03-22 12:32:13 +0530

    AboutUs page
    Updated the About us page with group members images

--- Frontend/src/AboutUs.js
+++ Frontend/src/AboutUs.js
@@ -1,62 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './AboutUs.css';
 
 const Team = () => {
-  const [activeMember, setActiveMember] = useState(null);
-
   const teamMembers = [
     {
       id: 1,
-      name: 'John Doe',
-      position: 'Software Engineer',
-      bio: 'John is a software engineer with 5 years of experience in web development.',
-      imgSrc: 'https://picsum.photos/id/1015/200/300',
+      name: 'Imashi Thakshila',
+      imgSrc: 'Imashi.jpg',
+      position:'Leader'
     },
     {
       id: 2,
-      name: 'Jane Smith',
-      position: 'Product Manager',
-      bio: 'Jane is a product manager with 7 years of experience in product management and project management.',
-      imgSrc: 'https://picsum.photos/id/1018/200/300',
+      name: 'Tharushi Dilisha',
+      imgSrc: 'Tharushi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Akash Perera',
+      imgSrc: 'Akash.jpg',
+    },
+    {
+      id: 4,
+      name: 'Harini Tharupa',
+      imgSrc: 'Tharupa.jpg',
+    },
+    {
+      id: 5,
+      name: 'Nipuni Pathirana',
+      imgSrc: 'Nipuni.jpg',
     },
     // Add more team members here
   ];
 
-  const handleMouseEnter = (member) => {
-    setActiveMember(member);
-  };
-
-  const handleMouseLeave = () => {
-    setActiveMember(null);
-  };
 
   return (
     <div className="team">
       <h2>Meet Our Team</h2>
       <div className="team-members">
         {teamMembers.map((member) => (
-          <div
-            key={member.id}
-            className="team-member"
-            onMouseEnter={() => handleMouseEnter(member)}
-            onMouseLeave={handleMouseLeave}
-          >
-            <img src={member.imgSrc} alt={member.name} />
+          <div key={member.id} className="team-member">
+            <img style={{ width: '150px', height: '180px' }} src={member.imgSrc} alt={member.name} />
             <h3>{member.name}</h3>
             <p>{member.position}</p>
+            <p>{member.bio}</p>
           </div>
         ))}
       </div>
-      {activeMember && (
-        <div className="active-member">
-          <img src={activeMember.imgSrc} alt={activeMember.name} />
-          <h3>{activeMember.name}</h3>
-          <p>{activeMember.position}</p>
-          <p>{activeMember.bio}</p>
-        </div>
-      )}
     </div>
   );
 };
 
-export default Team;
+
+export default Team
