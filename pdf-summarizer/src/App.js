9e539194c804ea703248d4f07ba076e3b8d8e92c
Nipuni Pathirana <nipuni.20220370@iit.ac.lk>
2024-03-23 22:17:25 +0530

    set API and update app.js

--- pdf-summarizer/src/App.js
+++ pdf-summarizer/src/App.js
@@ -1,8 +1,50 @@
-import { useState } from "react";
+import { useState } from "react";    
 import Logo from"./assets/Logo.png";
 
 function App() {
   const [value, setValue] = useState(null);
+  const[data, setData] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " +String(process.env.REACT_APP_OPENAI_KEY),
+      }, 
+      body: JSON.stringify({
+        prompt: value + `\n\nTl;dr`,
+        temperature: 0.1,
+        max_tokens: Math.floor(value.length / 2),
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.5, 
+        stop: ['"""'],
+        //aiModle :"gpt-3.5-turbo-1106"
+      }),
+    };
+    fetch("https://api.openai.com/v1/completions",
+    requestOptions)
+    .then((response)=> response.json())
+    .then((dt) => {
+    const text = dt.choices?.[0]?.text;
+      if (text) {
+        setSubmitting(false);
+        setData(text);
+      } else {
+        setSubmitting(false);
+      }
+  }) 
+  .catch ((error) => {
+    setSubmitting(false);
+    console.log(error);
+  });
+};
+
+console.log(data);
   return (
     <div className='w-full bg-[#0f172a] h-full min-h-[100vh 
     py-4 
@@ -24,8 +66,21 @@ function App() {
           rows={7} className="block w-full md:w-[650px] rounded-md border border-slate-700 bg-slate-800 p-2 text-sm shadow-lg font-medium text-white
           focus:border-gray-500 focus:outline-none focus:ring-0" onChange={(e) => setValue(e.target.value)}> 
           </textarea>
-          <button className="mt-5 bg-blue-500 px-5 py-2 text-white text-md font-semibold cursor-pointer rounded-md">Submit</button>
+          {value?.length > 0 && (submitting ? ( <p className="text-md text-cyan-500 mt-5">Please Wait .....</p>):(<button className="mt-5 bg-blue-500 px-5 py-2 text-white text-md font-semibold cursor-pointer rounded-md" onClick={handlesubmit}>Submit</button>))}
         </div>
+      </div>
+      <div className="w-full mt-10 flex flex-col gap-5 shadow-md items-center justify-center">{data?.length > 0 && 
+      <>
+      <p>Summary History</p>
+      {data?.map((d,index) => (
+        <div>
+
+          <p>{d}</p>
+        </div>
+      ))}
+      </>
+      }
+      
       </div>    
     </div>
   );
