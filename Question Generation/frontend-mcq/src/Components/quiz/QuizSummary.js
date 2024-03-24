347288dc3b2f60779223fde69bdf3f9b7dfb7649
Dilesha13 <136920648+13Tharushi@users.noreply.github.com>
2024-03-24 17:11:50 +0530

    Update QuizSummary.js

---
+++ Question Generation/frontend-mcq/src/Components/quiz/QuizSummary.js
@@ -0,0 +1,33 @@
+import React, { Component, Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+
+class QuizSummary extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            score: 0,
+            numberOfQuestions: 0,
+            numberOfAnsweredQuestions: 0,
+            correctAnswers: 0,
+            wrongAnswers: 0,
+            hintsUsed: 0,
+            fiftyFiftyUsed: 0
+        };
+    }
+
+    componentDidMount () {
+        const { state } = this.props.location;
+        if (state) {
+            this.setState({
+                score: (state.score / state.numberOfQuestions) * 100,
+                numberOfQuestions: state.numberOfQuestions,
+                numberOfAnsweredQuestions: state.numberOfAnsweredQuestions,
+                correctAnswers: state.correctAnswers,
+                wrongAnswers: state.wrongAnswers,
+                hintsUsed: state.hintsUsed,
+                fiftyFiftyUsed: state.fiftyFiftyUsed
+            });
+        }
+    }
+}
