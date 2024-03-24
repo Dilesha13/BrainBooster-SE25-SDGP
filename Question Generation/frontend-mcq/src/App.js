1dd76de23a8b68c5dcbace11bafe4dd01147267a
Dilesha13 <136920648+13Tharushi@users.noreply.github.com>
2024-03-24 22:27:50 +0530

    Updated App.js file

--- Question Generation/frontend-mcq/src/App.js
+++ Question Generation/frontend-mcq/src/App.js
@@ -5,3 +5,18 @@ import Home from './Components/Home';
 import QuizInstructions from './Components/quiz/QuizInstructions';
 import Play from './Components/quiz/Play';
 import QuizSummary from './Components/quiz/QuizSummary';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/play/instructions" element={<QuizInstructions />} />
+        <Route path="/play/quiz" element={<Play />} />
+        <Route path="/play/quizSummary" element={<QuizSummary />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
