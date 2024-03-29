45ee5f3226cc80cdc23c2ee3318ec321105ce658
Dilesha13 <158176367+akashperera142@users.noreply.github.com>
2024-03-29 09:48:14 +0530

    Initialize authentication service,firestore database

--- Profile/profile/src/App.js
+++ Profile/profile/src/App.js
@@ -19,3 +19,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const messagesRef = collection(firestore, 'messages');
+const orderedQuery = query(messagesRef, orderBy('createdAt', 'asc'));
