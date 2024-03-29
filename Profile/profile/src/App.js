7d07b7c585fd1d54d1919afe6ae1b7bc31949909
Dilesha13 <158176367+akashperera142@users.noreply.github.com>
2024-03-29 09:35:17 +0530

    Add Firebase configuration object

--- Profile/profile/src/App.js
+++ Profile/profile/src/App.js
@@ -6,3 +6,11 @@ import { getFirestore, collection, addDoc, serverTimestamp, query, orderBy } fro
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'; // Import storage functions
+
+// Firebase configuration object
+const firebaseConfig = {
+  apiKey: "AIzaSyBX1uu7oUaNZHg0WD6C9mls7SoHQTPIs98",
+  authDomain: "profile-eb12f.firebaseapp.com",
+  projectId: "profile-eb12f",
+  measurementId: "G-QMWJTB5KWK"
+};
