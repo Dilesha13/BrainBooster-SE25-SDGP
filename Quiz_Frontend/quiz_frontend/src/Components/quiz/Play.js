c1e4c2f77ac67fc8030a4ce4280d0f59b5978714
Dilesha13 <136920648+13Tharushi@users.noreply.github.com>
2024-03-30 18:31:38 +0530

    Update Play.js file

--- Quiz_Frontend/quiz_frontend/src/Components/quiz/Play.js
+++ Quiz_Frontend/quiz_frontend/src/Components/quiz/Play.js
@@ -1,26 +1,11 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Helmet } from 'react-helmet';
-import { Link } from 'react-router-dom';
+import M from 'materialize-css';
+import classnames from 'classnames';
 
-const Home = () => (
-    <Fragment>
-        <Helmet><title>Home - Quiz App</title></Helmet>
-        <div id="home">
-            <section>
-                <div style={{ textAlign: 'center' }}>
-                    <span className="mdi mdi-cube-outline cube"></span>
-                </div>
-                <h1>Welcome to BrainBooster Quiz</h1>
-                <div className="play-button-container">
-                    <ul>
-                        <li><Link className="play-button" to="/play/instructions">Play</Link></li>
-                    </ul>
-                </div>
+import questions from '../../questions.json';
+import isEmpty from '../../utils/is-empty';
 
-
-            </section>
-        </div>
-    </Fragment>
-);
-
-export default Home;
+import correctNotification from '../../assets/audio/correct-answer.mp3';
+import wrongNotification from '../../assets/audio/wrong-answer.mp3';
+import buttonSound from '../../assets/audio/button-sound.mp3';
