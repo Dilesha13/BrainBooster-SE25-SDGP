d955723993920a6fce3535c333856bdc38e1711e
Dilesha13 <136920648+13Tharushi@users.noreply.github.com>
2024-03-30 18:35:55 +0530

    Update Play.js file

--- Quiz_Frontend/quiz_frontend/src/Components/quiz/Play.js
+++ Quiz_Frontend/quiz_frontend/src/Components/quiz/Play.js
@@ -9,3 +9,29 @@ import isEmpty from '../../utils/is-empty';
 import correctNotification from '../../assets/audio/correct-answer.mp3';
 import wrongNotification from '../../assets/audio/wrong-answer.mp3';
 import buttonSound from '../../assets/audio/button-sound.mp3';
+
+class Play extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            questions,
+            currentQuestion: {},
+            nextQuestion: {},
+            previousQuestion: {},
+            answer: '',
+            numberOfQuestions: 0,
+            numberOfAnsweredQuestions: 0,
+            currentQuestionIndex: 0,
+            score: 0,
+            correctAnswers: 0,
+            wrongAnswers: 0,
+            hints: 5,
+            fiftyFifty: 2,
+            usedFiftyFifty: false,
+            nextButtonDisabled: false,
+            previousButtonDisabled: true,
+            previousRandomNumbers: [],
+            time: {}
+        };
+    }
+}
