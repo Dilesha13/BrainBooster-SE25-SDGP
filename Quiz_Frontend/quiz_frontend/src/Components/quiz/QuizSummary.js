6d09cd9d69c4678b443d19081b884f96bed378d9
Dilesha13 <136920648+13Tharushi@users.noreply.github.com>
2024-03-30 21:22:58 +0530

    Update QuizSummary.js file

---
+++ Quiz_Frontend/quiz_frontend/src/Components/quiz/QuizSummary.js
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+
+class QuizSummary extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            score: 0,
+            numberOfQuestions: 0,
+            numberOfAnsweredQuestions: 0,
+            correctAnswers: 0,
+            wrongAnswers: 0,
+            hintsUsed: 0,
+            fiftyFiftyUsed: 0
+        };
+    }
+
+    
+}
