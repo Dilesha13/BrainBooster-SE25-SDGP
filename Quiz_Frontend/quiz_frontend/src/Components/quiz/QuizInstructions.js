64c95b37544c4e901e1d19bb1ee6f000db69a450
Dilesha13 <136920648+13Tharushi@users.noreply.github.com>
2024-03-30 21:06:35 +0530

    Update QuizInstructions.js

--- Quiz_Frontend/quiz_frontend/src/Components/quiz/QuizInstructions.js
+++ Quiz_Frontend/quiz_frontend/src/Components/quiz/QuizInstructions.js
@@ -6,3 +6,33 @@ import answer from '../../assets/img/answer.png';
 import fiftyFifty from '../../assets/img/fiftyFifty.PNG';
 import hints from '../../assets/img/hints.PNG';
 import options from '../../assets/img/options.PNG';
+
+const QuizInstructions = () => (
+    <Fragment>
+        <Helmet><title>Quiz Instructions - Quiz App</title></Helmet>
+        <div className="instructions container">
+            <h1>How to do BrainBooster Quiz</h1>
+            <p>Ensure you read this guide from start to finish.</p>
+            <ul className="browser-default" id="main-list">
+                <li>The game has a duration of 15 minutes and ends as soon as your time elapses.</li>
+                <li>Each game consists of 15 questions.</li>
+                <li>
+                    Every question contains 4 options.
+                    
+                    <img src={options} alt="Quiz App options example" />
+                </li>
+                <li>
+                    Select the option which best answers the question by clicking (or selecting) it.
+                    <img src={answer} alt="Quiz App answer example" />
+                </li>
+
+            </ul>
+            <div>
+                <span className="left"><Link to="/">No take me back</Link></span>
+                <span className="right"><Link to="/play/quiz">Okay, Let's do this!</Link></span>
+            </div>
+        </div>
+    </Fragment>
+);
+
+export default QuizInstructions;
