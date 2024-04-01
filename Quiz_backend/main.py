56ec2c962ddbff0165743de237e1480c16138916
Dilesha13 <127937605+nipunipathirana@users.noreply.github.com>
2024-04-01 19:44:17 +0530

    Update get_keywords function

--- Quiz_backend/main.py
+++ Quiz_backend/main.py
@@ -216,3 +216,19 @@ def get_keywords(nlp,text,max_keywords,s2v,fdist,normalized_levenshtein,no_of_se
     keywords = sorted(keywords, key=lambda x: fdist[x])
     keywords = filter_phrases(keywords, max_keywords,normalized_levenshtein )
 
+    phrase_keys = get_phrases(doc)
+    filtered_phrases = filter_phrases(phrase_keys, max_keywords,normalized_levenshtein )
+
+    total_phrases = keywords + filtered_phrases
+
+    total_phrases_filtered = filter_phrases(total_phrases, min(max_keywords, 2*no_of_sentences),normalized_levenshtein )
+
+
+    answers = []
+    for answer in total_phrases_filtered:
+        if answer not in answers and MCQs_available(answer,s2v):
+            answers.append(answer)
+
+    answers = answers[:max_keywords]
+    
+    return answers
