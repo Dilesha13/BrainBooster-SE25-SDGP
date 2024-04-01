413abbbb44ce2134517ceee6f5ec4d84106fb7de
Dilesha13 <136920648+13Tharushi@users.noreply.github.com>
2024-04-01 19:06:37 +0530

    Update sense2vec_get_words function

--- Quiz_backend/main.py
+++ Quiz_backend/main.py
@@ -84,6 +84,16 @@ def sense2vec_get_words(word,s2v):
     most_similar = s2v.most_similar(sense, n=15)
 
     compare_list = [word_preprocessed]
+    for each_word in most_similar:
+        append_word = each_word[0].split("|")[0].replace("_", " ")
+        append_word = append_word.strip()
+        append_word_processed = append_word.lower()
+        append_word_processed = append_word_processed.translate(append_word_processed.maketrans("","", string.punctuation))
+        if append_word_processed not in compare_list and word_preprocessed not in append_word_processed and append_word_processed not in word_edits:
+            output.append(append_word.title())
+            compare_list.append(append_word_processed)
 
+
+    out = list(OrderedDict.fromkeys(output))
 
     return out
