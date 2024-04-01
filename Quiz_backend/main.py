97fdb373942accdea736c8ac49934d3ec4e3cd2e
Dilesha13 <136920648+13Tharushi@users.noreply.github.com>
2024-04-01 18:43:09 +0530

    Import libraries

--- Quiz_backend/main.py
+++ Quiz_backend/main.py
@@ -4,3 +4,11 @@ from joblib import load
 import numpy as np
 from PIL import Image
 import tensorflow as tf
+from typing import List
+from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from PIL import Image
+from io import BytesIO
+from PyPDF2 import PdfReader
+import nltk
+import spacy
