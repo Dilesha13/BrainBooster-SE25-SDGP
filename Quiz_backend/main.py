4b3eea75e82c1277e2099a8576588e664d58b691
Dilesha13 <158176367+akashperera142@users.noreply.github.com>
2024-04-01 20:28:35 +0530

    Update PythonPredictor class

--- Quiz_backend/main.py
+++ Quiz_backend/main.py
@@ -350,3 +350,9 @@ class PythonPredictor:
         self.normalized_levenshtein = NormalizedLevenshtein()
         self.set_seed(42)
         self.keyword_sentence_mapping= None
+                
+    def set_seed(self,seed):
+        numpy.random.seed(seed)
+        torch.manual_seed(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
