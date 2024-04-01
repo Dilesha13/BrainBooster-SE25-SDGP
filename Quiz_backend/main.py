48e586cab2371f5af6e3205f30f9c77982799a45
Dilesha13 <127937605+nipunipathirana@users.noreply.github.com>
2024-04-01 19:28:55 +0530

    update get_sentences_for_keyword function
    Iterating over keywords and extracting sentences

--- Quiz_backend/main.py
+++ Quiz_backend/main.py
@@ -122,3 +122,11 @@ def tokenize_sentences(text):
 def get_sentences_for_keyword(keywords, sentences):
     keyword_processor = KeywordProcessor()
     keyword_sentences = {}
+    for word in keywords:
+        word = word.strip()
+        keyword_sentences[word] = []
+        keyword_processor.add_keyword(word)
+    for sentence in sentences:
+        keywords_found = keyword_processor.extract_keywords(sentence)
+        for key in keywords_found:
+            keyword_sentences[key].append(sentence)
