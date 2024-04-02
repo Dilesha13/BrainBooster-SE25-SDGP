dac2d728cd234f13956386f8e00d2af93078e126
Dilesha13 <150505178+Tharu-20@users.noreply.github.com>
2024-04-02 13:17:25 +0530

    Created PDFuploader branch
    import required files to app.js file

---
+++ PDFuploader/pdfuploader/src/App.js
@@ -0,0 +1,12 @@
+// Plugins
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'; 
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+// Worker
+import { Worker } from '@react-pdf-viewer/core'; // install this library
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './index.css';
+
